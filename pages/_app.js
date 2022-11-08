03df68be0a359ffaba151c2f7125071af2db7c0b
thomaslawless <lawlessptom@gmail.com>
2022-11-08 15:18:56 -0500

    initial setup with userAuth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
 import '../styles/globals.css'
 
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+
+
+  return (
+
+    <SessionContextProvider
+
+      supabaseClient={supabaseClient}
+
+      initialSession={pageProps.initialSession}
+
+    >
+
+      <Component {...pageProps} />
+
+    </SessionContextProvider>
+
+  )
+
 }
 
 export default MyApp
